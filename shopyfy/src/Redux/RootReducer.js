dac79fd37dd3ed68d0f3caefcf6bbd00d61afc4f
MalikOwais123 <malikowaisjavaid@gmail.com>
2020-12-11 11:46:27 +0500

    integrated Redux

---
+++ shopyfy/src/Redux/RootReducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import authReducer from "./auth/authReducer";
+var RootReducer = combineReducers({
+  auth: authReducer,
+});
+
+export default RootReducer;
---
+++ shopyfy/src/Redux/auth/authReducer.js
@@ -0,0 +1,11 @@
+var initialState = null;
+var authReducer = (state = initialState,action ) =>{
+    var {type,payload} = action;
+    switch (type) {
+        default:
+
+            return state;
+    }
+}
+
+export default authReducer
